155fce4e3ae09784688a28a3586a424c366af043
jachingTH <jachingmaster@gmail.com>
2025-09-16 17:10:47 +0900

    LevelDesign
     - 모델링 = 3D AI 모델링 사이트에서 제작
     - 기본 맵 제작

---
+++ Source/ToTheDownStair/ToTheDownStairGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToTheDownStairGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AToTheDownStairGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AToTheDownStairGameMode();
+};
+
+
+
